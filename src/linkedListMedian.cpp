c6a16c1dea2f7894f5cc7aa86491ec8a207e5598
meshubh <meshubh@missionrnd.com>
2016-01-16 20:28:39 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -19,6 +19,39 @@ struct node {
 	struct node *next;
 };
 
-int linkedListMedian(struct node *head) {
-	return -1;
+int linkedListMedian(struct node *head)
+{
+	struct node *slow_ptr = head;
+	struct node *fast_ptr = head;
+	struct node* current = head;
+	int count = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	if (head != NULL)
+	{
+		while (fast_ptr != NULL && fast_ptr->next != NULL)
+		{
+			if (fast_ptr == head)
+				slow_ptr = head;
+			else
+				slow_ptr = slow_ptr->next;
+			fast_ptr = fast_ptr->next->next;
+		}
+		if (count % 2 == 1)
+		{
+			if (count == 1)
+				return head->num;
+			else
+				return slow_ptr->next->num;
+		}
+		else
+		{
+			return (slow_ptr->num + (slow_ptr->next->num)) / 2;
+		}
+	}
+	else
+		return -1;
 }
